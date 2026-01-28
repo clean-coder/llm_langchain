b564ded1e9e97f27d025319fb5fd032334da6781
clean-coder <jean-claude.brantschen@gmx.ch>
2026-01-28 10:33:20 +0100

    Add .env to .gitignore, implement GROQ API key handling, and update dependencies

---
+++ 1_fundamentals_request_groq.py
@@ -0,0 +1,29 @@
+import os
+from langchain_groq import ChatGroq
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv())
+import tools
+
+if os.getenv("GROQ_API_KEY") is None:
+    raise ValueError("GROQ_API_KEY not set")
+else:
+    print(f'GROQ_API_KEY loaded successfully: {os.getenv("GROQ_API_KEY")[:10]}...\n')
+
+# list of available models: https://console.groq.com/docs/models
+MODEL="openai/gpt-oss-120b"
+chat_model = ChatGroq(
+    model_name=MODEL,     
+    temperature=0.3,        
+    verbose=True, # set to False for production
+)
+
+response = chat_model.invoke("What is LangChain? Please in max 1 sentences.")
+
+tools.print_token_usage(response)
+
+print("---- Raw Output ----")
+print(type(response))
+print(response.content)
+
+print("\n---- JSON Output ----")
+print(tools.prettyfy_json(response.model_dump_json()))
--- tools.py
+++ tools.py
@@ -1,5 +1,5 @@
 import json
-
+from langchain_core.messages.ai import AIMessage
 
 def prettyfy_json(raw_json: str) -> str:
     # load json string into a Python object
@@ -13,3 +13,11 @@ def prettyfy_json(raw_json: str) -> str:
         ensure_ascii=False,       # keep Unicode characters instead of \uXXXX
         separators=(', ', ': ')   # pretty separators (comma+space, colon+space)
     )    
+
+
+def print_token_usage(response: AIMessage) -> None:
+    print("\n---- Token Usage ----")
+    print(f"Prompt Tokens: {response.response_metadata['token_usage']['prompt_tokens']}")
+    print(f"Completion Tokens: {response.response_metadata['token_usage']['completion_tokens']}")
+    print(f"Total Tokens: {response.response_metadata['token_usage']['total_tokens']}")
+    print()
