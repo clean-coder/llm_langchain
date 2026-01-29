901737b7d305c9a546a311ee03c2d65d1058cdb5
clean-coder <jean-claude.brantschen@gmx.ch>
2026-01-29 10:25:45 +0100

    Google gemini example request added

---
+++ 1_fundamentals_request_google.py
@@ -0,0 +1,19 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+import tools
+
+# list of models: https://ai.google.dev/gemini-api/docs/models
+
+MODEL = "gemini-2.5-flash-lite"
+chat_model = ChatGoogleGenerativeAI(
+    model=MODEL,  # Note: parameter is 'model' not 'model_name'
+    temperature=0.3,        
+    verbose=True, # set to False for production
+)
+
+response = chat_model.invoke("What is LangChain? Please in max 1 sentences.")
+print("---- Raw Output ----")
+print(type(response))
+print(response.content)
+
+print("\n---- JSON Output ----")
+print(tools.prettyfy_json(response.model_dump_json()))
