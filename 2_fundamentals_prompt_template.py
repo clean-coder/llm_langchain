ed928dfb0cfc880f4a56d34a4108d043c7d6a134
clean-coder <jean-claude.brantschen@gmx.ch>
2026-02-06 14:22:53 +0100

    Refactor response handling and remove unused tools import in fundamentals prompt template

--- 2_fundamentals_prompt_template.py
+++ 2_fundamentals_prompt_template.py
@@ -1,6 +1,5 @@
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
-import tools
 
 MODEL = "gpt-4o-mini"
 chat_model = ChatOpenAI(
@@ -16,10 +15,8 @@ prompt = ChatPromptTemplate.from_template(template)
 chain = prompt | chat_model
 print(f'Type of Chain: {type(chain)}')
 
-response = chain.invoke({"color":"red"})
-print("\n---- Raw Output ----")
-print(f'Type of response: {type(response)}')
-print(response.content)
+response_red = chain.invoke({"color":"red"})
+print(response_red.content)
 
-print("\n---- JSON Output ----")
-print(tools.prettyfy_json(response.model_dump_json()))
+response_blue = chain.invoke({"color":"blue"})
+print(response_blue.content)
