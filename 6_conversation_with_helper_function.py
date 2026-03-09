aa983dba676d73fcc70f81d31936bd8c79f693e4
clean-coder <jean-claude.brantschen@gmx.ch>
2026-03-09 09:45:21 +0100

    Refactor chat function to remove redundant LLM parameter and streamline invocation

--- 6_conversation_with_helper_function.py
+++ 6_conversation_with_helper_function.py
@@ -4,20 +4,19 @@ from langchain_core.output_parsers import StrOutputParser
 
 chat_history = []
 
-def chat(user_message, chat_history, llm):
+def chat(user_message, chat_history):
+    llm = ChatOllama(model="llama3.1")
     chat_history.append(HumanMessage(content=user_message))
     response = llm.invoke(chat_history)
     chat_history.append(AIMessage(content=response.content))
     return response.content
 
 if __name__ == "__main__":
-    llm = ChatOllama(model="llama3.1")
-
     print("---- First Question ----")
-    print(chat("What is the capital of France?", chat_history, llm))
+    print(chat("What is the capital of France?", chat_history))
 
     print("\n\n---- Second Question ----")
-    print(chat("And Sweden?", chat_history, llm))
+    print(chat("And Sweden?", chat_history))
 
     print("\n\n---- Third Question ----")
-    print(chat("Which one is larger?", chat_history, llm))
+    print(chat("Which one is larger?", chat_history))
